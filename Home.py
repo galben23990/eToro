6c595df4d59ec5e15d161bf7915998d2fe51b112
galben23990 <153602946+galben23990@users.noreply.github.com>
2024-01-07 12:44:38 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,16 +1,1 @@
-import streamlit as st
 
-# Set page configuration
-st.set_page_config(page_title='My first app', page_icon="🧊", layout='wide')
-
-# Center-align the title
-st.title('Mosaic Tool')
-st.markdown("<h1 style='text-align: center;'>Hello eToro!</h1>", unsafe_allow_html=True)
-
-# Center-align the GIF image
-st.markdown(
-    "<div style='display: flex; justify-content: center;'>"
-    "<img src='https://media.giphy.com/media/3o6vXTpomeZEyxufGU/giphy.gif' alt='GIF Image'/>"
-    "</div>",
-    unsafe_allow_html=True
-)
